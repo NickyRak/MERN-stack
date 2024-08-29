f67922f80b999248a6ae79d0f23130ca292377f2
Nicky RAKOTOARIMANANA <nickyrakotoarimanana@gmail.com>
2024-08-29 19:31:29 +0200

    Setup MongoDB & Mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
+const mongoose = require("mongoose");
 const workoutRouters = require("./routes/workouts");
 
 // express app
@@ -17,9 +18,17 @@ app.use((req, res, next) => {
 // routes
 app.use("/api/workouts", workoutRouters);
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-  console.log("Server started on port 4000...");
-});
+// connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log("Connected to the db & listening on port", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 process.env;
