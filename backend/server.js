806b2a56cca4f1b2a349127128228859008b1a27
Nicky RAKOTOARIMANANA <nickyrakotoarimanana@gmail.com>
2024-08-29 18:53:29 +0200

    Express Router & API Routes

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,21 @@
 require("dotenv").config();
 
 const express = require("express");
+const workoutRouters = require("./routes/workouts");
 
 // express app
 const app = express();
 
-// midleware
+// middleware
+app.use(express.json());
+
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
 
 // routes
-app.get("/", (req, res) => {
-  res.json({ msg: "Hello from server!" });
-});
+app.use("/api/workouts", workoutRouters);
 
 // listen for requests
 app.listen(process.env.PORT, () => {
